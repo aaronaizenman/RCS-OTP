342562c5b6a4e7a30a9c42526a062b34c03be65f
Aaron Aizenman <aizenman.aaron@gmail.com>
2022-09-01 15:59:12 -0400

    create new button

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 function userpass_submit(user, pass) {
-    console.log("submit button pressed true")
-    console.log(user)
-    console.log(pass)
-    
+    console.log("submit button pressed true");
+    console.log(user);
+    console.log(pass);
+
+    let btn = document.createElement("button");
+    btn.innerHTML = "Send 2FA code to my phone.";
+    document.body.appendChild(btn);
+
     return true
 }
